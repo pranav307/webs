7241916851ba1b7d0795dcfb49ef4d85e12e558b
pranav307 <psgusain307@gmail.com>
2024-11-14 18:11:28 +0530

    clients

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server:{
+      proxy:{
+        '/api': 'http://localhost:8000',
+      },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    },
+  },
+});
