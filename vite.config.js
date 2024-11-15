390b7ce23eafafbb1efe4627bc3aa62fb59e371c
pranav307 <psgusain307@gmail.com>
2024-11-15 11:52:45 +0530

    clientss

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-      proxy:{
-        '/api': 'http://localhost:8000',
-      },
+  root: path.resolve(__dirname, 'amazon'), // Adjust the path if necessary
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000',
+    },
   },
   plugins: [react()],
   resolve: {
